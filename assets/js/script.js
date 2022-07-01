87d827724b59184aaf7276df4127d5074a33de51
AR Rasel <rasel403@gmail.com>
2022-07-01 16:31:57 +0600

    Taxi booking site

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Custom JS code here
+
+const el = document.querySelector(".banner_gradient1");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
